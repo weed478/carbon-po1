c0aa79d933b4abb5b10ca4cf812f147c932251a7
Carbon225 <carbon225@protonmail.com>
2021-10-11 13:31:39 +0200

    initial

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,10 @@
+package agh.ics.oop;
+
+import static java.lang.System.out;
+
+public class World {
+    public static void main(String[] args) {
+        out.println("Start");
+        out.println("Stop");
+    }
+}
