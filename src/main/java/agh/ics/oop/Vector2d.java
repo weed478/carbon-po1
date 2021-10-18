77d1145e40b87f43e130173fb29e17e487e71fd7
Carbon225 <carbon225@protonmail.com>
2021-10-18 13:20:52 +0200

    lab2.5 vector

---
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,65 @@
+package agh.ics.oop;
+
+import java.util.Objects;
+
+public class Vector2d {
+    public final int x;
+    public final int y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ')';
+    }
+
+    boolean precedes(Vector2d o) {
+        return x <= o.x && y <= o.y;
+    }
+
+    boolean follows(Vector2d o) {
+        return x <= o.x && y <= o.y;
+    }
+
+    Vector2d upperRight(Vector2d o) {
+        return new Vector2d(
+                Math.max(x, o.x),
+                Math.max(y, o.y)
+        );
+    }
+
+    Vector2d lowerLeft(Vector2d o) {
+        return new Vector2d(
+                Math.min(x, o.x),
+                Math.min(y, o.y)
+        );
+    }
+
+    Vector2d add(Vector2d o) {
+        return new Vector2d(x + o.x, y + o.y);
+    }
+
+    Vector2d subtract(Vector2d o) {
+        return add(o.opposite());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector2d vector2d = (Vector2d) o;
+        return x == vector2d.x && y == vector2d.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    Vector2d opposite() {
+        return new Vector2d(-x, -y);
+    }
+}
