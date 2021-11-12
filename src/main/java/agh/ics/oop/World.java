ab249871b5d27773bf106a17324b5ee1781646e5
Carbon225 <carbon225@protonmail.com>
2021-11-12 20:53:38 +0100

    list

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,14 +1,15 @@
 package agh.ics.oop;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class World {
     public static void main(String[] args) {
         Stream<MoveDirection> directions = OptionsParser.parse(Arrays.stream(args));
         IWorldMap map = new RectangularMap(10, 5);
-        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
-        IEngine engine = new SimulationEngine(directions, map, Arrays.asList(positions));
+        List<Vector2d> positions = Arrays.asList(new Vector2d(2,2), new Vector2d(3,4));
+        IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
     }
 }
