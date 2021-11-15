062d163a9cecff987e2dbb85bc595dc7388aeaa3
Carbon225 <carbon225@protonmail.com>
2021-11-15 14:11:41 +0100

    test grass field

---
+++ src/test/java/agh/ics/oop/GrassFieldTest.java
@@ -0,0 +1,27 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrassFieldTest {
+    @Test
+    void testMovement() {
+        IWorldMap map = new GrassField(10);
+        Animal animal = new Animal(map, new Vector2d(2, 2));
+
+        assertTrue(map.place(animal));
+
+        assertTrue(map.isOccupied(new Vector2d(2, 2)));
+        assertEquals(animal, map.objectAt(new Vector2d(2, 2)));
+        assertFalse(map.canMoveTo(new Vector2d(2, 2)));
+        assertTrue(map.canMoveTo(new Vector2d(2, 3)));
+
+        animal.move(MoveDirection.FORWARD);
+
+        assertTrue(map.isOccupied(new Vector2d(2, 3)));
+        assertEquals(animal, map.objectAt(new Vector2d(2, 3)));
+        assertTrue(map.canMoveTo(new Vector2d(2, 2)));
+        assertFalse(map.canMoveTo(new Vector2d(2, 3)));
+    }
+}
