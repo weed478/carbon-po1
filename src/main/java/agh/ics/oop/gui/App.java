bd658a1eae8abd6ece0aa4cd972204ad7ed26dc8
Carbon225 <carbon225@protonmail.com>
2021-12-06 19:28:46 +0100

    Gridalator

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -2,11 +2,10 @@ package agh.ics.oop.gui;
 
 import agh.ics.oop.*;
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,8 +16,10 @@ public class App extends Application {
 
     private final AbstractWorldMap map = new GrassField(10);
     private IEngine engine;
+    private final Gridalator gridalator = new Gridalator(map);
 
     private GridPane gridPane;
+    private Pane rootPane;
 
     @Override
     public void init() {
@@ -43,26 +44,18 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         VBox root = new VBox();
+        rootPane = root;
 
         Button stepButton = new Button("Move");
         stepButton.setOnAction(e -> simulateStep());
         root.getChildren().add(stepButton);
 
-        gridPane = new GridPane();
-        gridPane.setGridLinesVisible(true);
         drawGrid();
-        root.getChildren().add(gridPane);
 
         Scene scene = new Scene(root, 400, 400);
 
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    private void clearGrid() {
-        Node node = gridPane.getChildren().get(0);
-        gridPane.getChildren().clear();
-        gridPane.getChildren().add(0, node);
     }
 
     private void simulateStep() {
@@ -71,30 +64,10 @@ public class App extends Application {
     }
 
     private void drawGrid() {
-        clearGrid();
-
-        Rect bounds = map.getDrawingBounds();
-
-        for (int x = bounds.getBL().x; x <= bounds.getTR().x; x++) {
-            for (int y = bounds.getBL().y; y <= bounds.getTR().y; y++) {
-                Vector2d mapPos = new Vector2d(x, y);
-                Vector2d gridPos = mapToGrid(mapPos, bounds);
-                Object o = map.objectAt(mapPos);
-                Label field;
-                if (o != null) {
-                    field = new Label(o.toString());
-                }
-                else {
-                    field = new Label("    ");
-                }
-                gridPane.add(field, gridPos.x, gridPos.y);
-            }
+        if (gridPane != null) {
+            rootPane.getChildren().remove(gridPane);
         }
-    }
-
-    private static Vector2d mapToGrid(Vector2d v, Rect bounds) {
-        v = v.subtract(bounds.getBL());
-        v = new Vector2d(v.x, bounds.getTR().y - bounds.getBL().y - v.y);
-        return v;
+        gridPane = gridalator.makeGrid();
+        rootPane.getChildren().add(gridPane);
     }
 }
