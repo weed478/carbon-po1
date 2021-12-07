7afbff35356aae226df1b55a69b19d29346ba2b2
Carbon225 <carbon225@protonmail.com>
2021-12-07 16:44:03 +0100

    change arg parsing

--- src/main/java/agh/ics/oop/AppLaunchIntent.java
+++ src/main/java/agh/ics/oop/AppLaunchIntent.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public abstract class AppLaunchIntent {
 
     public static AppLaunchIntent parse(List<String> args) {
-        if (args.size() == 1 && args.get(0).equals("skynet")) {
+        if (args.size() > 0 && args.get(args.size() - 1).equals("skynet")) {
             return new SkynetLaunchIntent();
         } else {
             Stream<MoveDirection> directions = OptionsParser.parse(args.stream());
