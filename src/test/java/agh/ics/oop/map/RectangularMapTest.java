06a051e1b17941c9d9dbf41fea536df50c616f9c
Carbon225 <carbon225@protonmail.com>
2021-12-10 20:29:58 +0100

    update move tests

--- src/test/java/agh/ics/oop/map/RectangularMapTest.java
+++ src/test/java/agh/ics/oop/map/RectangularMapTest.java
@@ -1,6 +1,5 @@
 package agh.ics.oop.map;
 
-import agh.ics.oop.core.MoveDirection;
 import agh.ics.oop.core.Rect;
 import agh.ics.oop.core.Vector2d;
 import agh.ics.oop.objects.Animal;
@@ -15,19 +14,19 @@ class RectangularMapTest {
         RectangularMap map = new RectangularMap(new Rect(-10, -10, 11, 11), new Rect(-2, -2, 3, 3));
         assertEquals(0, map.getAnimalsAt(new Vector2d(0, 0)).size());
 
-        Animal animal = new Animal(map, new Vector2d(0, 0), MapDirection.NORTH, 1);
+        Animal animal = new Animal(map, new Vector2d(0, 0), MapDirection.N, 1);
         assertEquals(1, map.getAnimalsAt(new Vector2d(0, 0)).size());
         assertEquals(0, map.getAnimalsAt(new Vector2d(0, 1)).size());
 
-        animal.move(MoveDirection.FORWARD);
+        animal.move(0);
         assertEquals(0, map.getAnimalsAt(new Vector2d(0, 0)).size());
         assertEquals(1, map.getAnimalsAt(new Vector2d(0, 1)).size());
 
-        animal.move(MoveDirection.RIGHT);
+        animal.move(2);
         assertEquals(0, map.getAnimalsAt(new Vector2d(1, 1)).size());
         assertEquals(1, map.getAnimalsAt(new Vector2d(0, 1)).size());
 
-        animal.move(MoveDirection.FORWARD);
+        animal.move(0);
         assertEquals(1, map.getAnimalsAt(new Vector2d(1, 1)).size());
         assertEquals(0, map.getAnimalsAt(new Vector2d(0, 1)).size());
     }
