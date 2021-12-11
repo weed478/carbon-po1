693176e422ba374bd4ffdf82e330379cabe419a1
Carbon225 <carbon225@protonmail.com>
2021-12-11 16:54:36 +0100

    remove grid coord labels

--- src/main/java/agh/ics/oop/gui/GridBuilder.java
+++ src/main/java/agh/ics/oop/gui/GridBuilder.java
@@ -4,7 +4,6 @@ import agh.ics.oop.core.Rect;
 import agh.ics.oop.core.Vector2d;
 import javafx.geometry.HPos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -26,25 +25,18 @@ public class GridBuilder {
 
         GridPane gridPane = new GridPane();
 
-        for (int i = 0; i < bounds.width() + 1; i++) {
+        for (int i = 0; i < bounds.width(); i++) {
             gridPane.getColumnConstraints().add(new ColumnConstraints(GRID_SIZE));
         }
 
-        for (int i = 0; i < bounds.height() + 1; i++) {
+        for (int i = 0; i < bounds.height(); i++) {
             gridPane.getRowConstraints().add(new RowConstraints(GRID_SIZE));
-        }
-
-        for (int x = bounds.getBL().x, xGrid = 1; x < bounds.getTR().x; x++, xGrid++) {
-            gridPane.add(makeLabel(String.valueOf(x)), xGrid, 0);
-        }
-        for (int y = bounds.getTR().y - 1, yGrid = 1; y >= bounds.getBL().y; y--, yGrid++) {
-            gridPane.add(makeLabel(String.valueOf(y)), 0, yGrid);
         }
 
         for (int x = bounds.getBL().x; x < bounds.getTR().x; x++) {
             for (int y = bounds.getBL().y; y < bounds.getTR().y; y++) {
                 Vector2d mapPos = new Vector2d(x, y);
-                Vector2d gridPos = mapToGrid(mapPos).add(new Vector2d(1, 1));
+                Vector2d gridPos = mapToGrid(mapPos);
 
                 List<IDrawableElement> drawables = map.getDrawablesAt(mapPos);
 
@@ -70,11 +62,5 @@ public class GridBuilder {
         v = v.subtract(map.getDrawingBounds().getBL());
         v = flipY(v);
         return v;
-    }
-
-    private static Label makeLabel(String text) {
-        Label field = new Label(text);
-        GridPane.setHalignment(field, HPos.CENTER);
-        return field;
     }
 }
