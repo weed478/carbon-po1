4b48607fbae21f0ec11ee6b1aa6099813767c4d1
Carbon225 <carbon225@protonmail.com>
2021-12-24 00:33:56 +0100

    don't catch in main

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -6,11 +6,6 @@ import javafx.application.Application;
 public class World {
 
     public static void main(String[] args) {
-        try {
-            Application.launch(App.class, args);
-        }
-        catch (Exception e) {
-            System.err.println("Kółko graniaste, czworokanciaste, " + e.getMessage() + ", a my wszyscy bęc.");
-        }
+        Application.launch(App.class, args);
     }
 }
