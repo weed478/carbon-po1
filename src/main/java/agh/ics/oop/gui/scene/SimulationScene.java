9a9a571e02b87af8154c5c74c4441685cca5f292
Carbon225 <carbon225@protonmail.com>
2021-12-25 18:46:29 +0100

    black seperator

--- src/main/java/agh/ics/oop/gui/scene/SimulationScene.java
+++ src/main/java/agh/ics/oop/gui/scene/SimulationScene.java
@@ -5,12 +5,13 @@ import agh.ics.oop.gui.controller.SimulationController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class SimulationScene {
 
     public SimulationScene(Stage stage, SimulationConfig config) {
-        HBox root = new HBox(20);
+        HBox root = new HBox(9);
 
         FXMLLoader loaderLeft = new FXMLLoader(getClass().getResource("simulation.fxml"));
         FXMLLoader loaderRight = new FXMLLoader(getClass().getResource("simulation.fxml"));
@@ -18,8 +19,15 @@ public class SimulationScene {
         loaderLeft.setControllerFactory(t -> new SimulationController(config, true, false));
         loaderRight.setControllerFactory(t -> new SimulationController(config, false, false));
 
+        Pane separator = new Pane();
+        separator.setMinWidth(2);
+        separator.setPrefWidth(2);
+        separator.setMaxWidth(2);
+        separator.setStyle("-fx-background-color: black");
+
         try {
             root.getChildren().add(loaderLeft.load());
+            root.getChildren().add(separator);
             root.getChildren().add(loaderRight.load());
         } catch (Exception e) {
             e.printStackTrace();
