85d4bc67d09737d883ff59f48fd1f1680a516868
Carbon225 <carbon225@protonmail.com>
2021-12-25 19:31:42 +0100

    save average

--- src/main/java/agh/ics/oop/gui/StatsSaver.java
+++ src/main/java/agh/ics/oop/gui/StatsSaver.java
@@ -51,8 +51,15 @@ public class StatsSaver {
     }
 
     private void saveFile(File f, List<SimulationStatistics> stats) throws IOException {
+        double averageAnimals = 0;
+        double averageGrass = 0;
+        double averageEnergy = 0;
+        double averageLifetime = 0;
+        double averageChildren = 0;
+
         FileWriter writer = new FileWriter(f);
         writer.write("day,animals,plants,average energy,average lifetime,average children\n");
+
         for (SimulationStatistics s : stats) {
             writer.write(String.valueOf(s.day));
             writer.write(",");
@@ -71,9 +78,36 @@ public class StatsSaver {
 
             writer.write(roundDigits(s.averageChildren));
             writer.write("\n");
+
+            averageAnimals += s.numAnimals;
+            averageGrass += s.numGrass;
+            averageEnergy += s.averageFood;
+            averageLifetime += s.averageLifetime;
+            averageChildren += s.averageChildren;
         }
 
+        averageAnimals /= stats.size();
+        averageGrass /= stats.size();
+        averageEnergy /= stats.size();
+        averageLifetime /= stats.size();
+        averageChildren /= stats.size();
 
+        writer.write(",");
+
+        writer.write(roundDigits(averageAnimals));
+        writer.write(",");
+
+        writer.write(roundDigits(averageGrass));
+        writer.write(",");
+
+        writer.write(roundDigits(averageEnergy));
+        writer.write(",");
+
+        writer.write(roundDigits(averageLifetime));
+        writer.write(",");
+
+        writer.write(roundDigits(averageChildren));
+        writer.write("\n");
 
         writer.close();
     }
